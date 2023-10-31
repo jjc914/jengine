4ad4dae5a5ce76fd217617d8f70c1defcae479c6
Joshua Chasnov <jjc914@student.cis.edu.hk>
2023-10-31 11:03:10 -0700

    removed

--- build/CMakeFiles/jengine.dir/src/engine/engine.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/jengine.dir/src/engine/engine.cpp.o: \
-  /Users/joshuachasnov/Documents/ProgrammingProjects/CppProjects/jengine/src/engine/engine.cpp \
-  /Users/joshuachasnov/Documents/ProgrammingProjects/CppProjects/jengine/src/engine/engine.hpp
--- build/lib/util-lib/src/CMakeFiles/utilib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libutilib.dylib"
-  "libutilib.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/utilib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
